e9131327803e4fcb8d05010383a187f79494ad75
landonrex <104240732+landonrex@users.noreply.github.com>
2023-08-19 17:15:56 +0800

    Add files via upload

---
+++ 打下最紮實AI基礎不依賴套件機器學習神經網路/程式/隨機數據的線性回歸和標準化_甯.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error
+
+# 生成更多的隨機數據
+np.random.seed(0)
+num_samples = 50
+population = np.random.randint(100, 1000, num_samples)
+profit = 20 + 0.05 * population + np.random.normal(0, 30, num_samples)
+
+data = {
+    'Population': population,
+    'Profit': profit
+}
+
+df = pd.DataFrame(data)
+
+# 提取特徵和標籤
+X = df[['Population']]
+y = df['Profit']
+
+# 線性回歸模型
+model = LinearRegression()
+model.fit(X, y)
+
+# 預測
+predictions = model.predict(X)
+
+# 原始數據散點圖和線性回歸線
+plt.scatter(X, y, label='Original Data')
+plt.plot(X, predictions, color='red', label='Linear Regression')
+plt.xlabel('Population')
+plt.ylabel('Profit')
+plt.legend()
+plt.title('Linear Regression: Population vs Profit')
+plt.show()
+
+# 標準化數據
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+# 標準化數據散點圖
+plt.scatter(X_scaled, y, label='Standardized Data')
+plt.xlabel('Standardized Population')
+plt.ylabel('Profit')
+plt.legend()
+plt.title('Standardized Data: Population vs Profit')
+plt.show()
+
+# 再次進行線性回歸
+model_scaled = LinearRegression()
+model_scaled.fit(X_scaled, y)
+
+# 預測
+predictions_scaled = model_scaled.predict(X_scaled)
+
+# 標準化數據線性回歸線
+plt.scatter(X_scaled, y, label='Standardized Data')
+plt.plot(X_scaled, predictions_scaled, color='red', label='Linear Regression')
+plt.xlabel('Standardized Population')
+plt.ylabel('Profit')
+plt.legend()
+plt.title('Linear Regression with Standardized Data: Population vs Profit')
+plt.show()
+
+# 計算標準化數據的MSE
+mse_scaled = mean_squared_error(y, predictions_scaled)
+print(f'Standardized Data MSE: {mse_scaled:.2f}')
